a83b1b95ddec1f76712b2bf31ec8f3600a0992e9
silverwing1212 <adambarton1212@gmail.com>
2023-11-29 05:59:56 -0500

    created admin model registration draft

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
+from .models import CarModel, CarMake
 # from .models import related models
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 10
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    model = CarModel
+    inlines = [CarModelInline]
+    extra = 10
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    model = CarMake
 
 # Register models here
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake, CarMakeAdmin)
