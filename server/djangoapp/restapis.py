4ccfa3d7cd6a560742e29c448981fbaa0fa10c65
silverwing1212 <adambarton1212@gmail.com>
2023-12-04 05:31:35 -0500

    added dealer frontend views

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -4,6 +4,11 @@ import json
 from .models import CarDealer
 from requests.auth import HTTPBasicAuth
 
+credentials = {
+    "IAM_API_KEY": "4iKJcQaT1ZJq6tbnwC4TZIqUwAoLz4UjLA7RBEm01OGN",
+    "COUCH_URL": "https://3c366aa0-e65a-45df-b3a8-94d6d0482251-bluemix.cloudantnosqldb.appdomain.cloud"
+}
+
 
 # Create a `get_request` to make HTTP GET requests
 # e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
@@ -32,13 +37,18 @@ def post_request(url, json_payload, **kwargs):
     try:
         # Call get method of requests library with URL and parameters
         response = requests.post(url, headers={'Content-Type': 'application/json'}, params=kwargs, json=json_payload)
+        status_code = response.status_code
+        print("With status {} ".format(status_code))
+        json_data = json.loads(response.text)
+        return json_data
     except:
         # If any error occurs
         print("Network exception occurred")
-    status_code = response.status_code
-    print("With status {} ".format(status_code))
-    json_data = json.loads(response.text)
-    return json_data
+        return {}
+
+# Cloud Function
+def cloud_function(url, json_payload, **kwargs):
+    return post_request(url, json_payload)["response"]["result"]
 
 # Create a get_dealers_from_cf method to get dealers from a cloud function
 # def get_dealers_from_cf(url, **kwargs):
@@ -47,7 +57,7 @@ def post_request(url, json_payload, **kwargs):
 def get_dealers_from_cf(url, **kwargs):
     results = []
     # Call get_request with a URL parameter
-    json_result = get_request(url)
+    """json_result = cloud_function(url, credentials)
     if json_result:
         # Get the row list in JSON as dealers
         dealers = json_result["rows"]
@@ -60,14 +70,13 @@ def get_dealers_from_cf(url, **kwargs):
                                    id=dealer_doc["id"], lat=dealer_doc["lat"], long=dealer_doc["long"],
                                    short_name=dealer_doc["short_name"],
                                    st=dealer_doc["st"], zip=dealer_doc["zip"])
-            results.append(dealer_obj)
-
+            results.append(dealer_obj)"""
     return results
 
 
 
 # Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
-def get_dealer_reviews_from_cf(url, dealerId=dealerId):
+def get_dealer_reviews_from_cf(url, dealerId):
     # TODO (XXX):
     # Contrive CF GET REQUEST URL
     # Make Cloud Functions + Test Them on Postman
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
 # from .restapis import related methods
-from .restapis import get_dealer_reviews_from_cf
+from .restapis import get_dealer_reviews_from_cf, get_dealers_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -88,20 +88,34 @@ def registration_request(request):
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     if request.method == "GET":
+        context = {}
         url = "your-cloud-function-domain/dealerships/dealer-get"
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
         # Concat all dealer's short name
         dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
         # Return a list of dealer short name
-        return HttpResponse(dealer_names)
+        context["dealership_list"] = [
+            { "id": "0", "full_name": "Adam Zachary", "city": "Austin", "address": "X", "zip": 12345, "state": "Texas"},
+            { "id": "1", "full_name": "Bianca Yuo", "city": "NYC", "address": "X", "zip": 12345, "state": "New York"},
+            { "id": "2", "full_name": "Caesar Xi", "city": "NYC", "address": "X", "zip": 12345, "state": "Texas"},
+            { "id": "3", "full_name": "Diana Wes", "city": "Utah", "address": "X", "zip": 12345, "state": "Utah"},
+        ]
+        return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
-def get_dealer_details(requests, dealer_id):
-    get_dealer_reviews_from_cf()
+def get_dealer_details(request, dealer_id):
+    context = {}
+    # get_dealer_reviews_from_cf
+    context["review_list"] = [
+        { "id": "0", "sentiment": "positive", "car_make": "X", "car_model": "Y", "purchase_year": "Z", "content": "Line 1\nLine2\nLine 3"},
+        { "id": "1", "sentiment": "neutral", "car_make": "X", "car_model": "Y", "purchase_year": "Z", "content": "Line 1\nLine2\nLine 3"},
+        { "id": "2", "sentiment": "negative", "car_make": "X", "car_model": "Y", "purchase_year": "Z", "content": "Line 1\nLine2\nLine 3"},
+    ]
+    return render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
